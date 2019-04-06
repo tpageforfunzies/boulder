f6a48225b7dc81921f9dea151c5706a20511b588
Taylor Page <tpage@Taylors-MacBook-Pro.local>
2019-04-06 02:19:35 -0500

    dddoooccckkkeeerrrr deploy

--- dockerdeploy.sh
+++ dockerdeploy.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# update repo
 git pull
+# compile mac and linux
 make build
-docker ps -a
+# stop and delete old container
+docker stop boulderapp && docker rm boulderapp
+# build new image
+docker build -t boulderlinux .
+# create new container
+docker create -it --name boulderapp -p 80:80 boulderlinux:latest
+# start that puppy up
+docker start boulderapp
