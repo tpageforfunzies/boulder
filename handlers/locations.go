f11bad0a408017c5fa76a54557b0a7af9bce8903
Taylor Page <taylor.jay.page@gmail.com>
2019-05-12 22:31:18 -0500

    add location model

---
+++ handlers/locations.go
@@ -0,0 +1,13 @@
+// locations.go
+package handlers
+
+// import (
+// 	u "github.com/tpageforfunzies/boulder/common"
+// 	"github.com/tpageforfunzies/boulder/models"
+// 	"github.com/tpageforfunzies/boulder/services"
+// 	"encoding/json"
+// 	"github.com/gin-gonic/gin"
+// 	"net/http"
+// 	"strconv"
+// )
+
