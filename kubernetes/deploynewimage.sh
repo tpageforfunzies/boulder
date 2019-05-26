f97dbd691e39c27a8ce1b93030288d7765bf8e44
tpage <taylor.jay.page@gmail.com>
2019-05-25 22:42:58 -0500

    perma tag 1.2 for a while for CD

--- kubernetes/deploynewimage.sh
+++ kubernetes/deploynewimage.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # call it with bash ./kubernetes/deploynewimage.sh X.XX to build, push and update the deployment
-if [ -z "$1" ]; then
-    echo "gotta have a version number dog"
-    exit 2
-fi
+# if [ -z "$1" ]; then
+#     echo "gotta have a version number dog"
+#     exit 2
+# fi
 echo "======building image======"
-docker build -t tpageforfunzies/boulderarmhf:v"$1" .
+docker build -t tpageforfunzies/boulderarmhf:v1.2 .
 echo "======pushing image======"
-docker push tpageforfunzies/boulderarmhf:v"$1"
+docker push tpageforfunzies/boulderarmhf:v1.2
 echo "======updating deployment to rollout new image======"
-kubectl --record deployment.apps/bouldertracker set image deployment.v1.apps/bouldertracker bouldertracker-api=tpageforfunzies/boulderarmhf:v"$1"
+kubectl --record deployment.apps/bouldertracker set image deployment.v1.apps/bouldertracker bouldertracker-api=tpageforfunzies/boulderarmhf:v1.2
